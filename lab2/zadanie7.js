56e58e45a76527de1075a56b857a41c2ecc4c399
Angelika Prass <a.prass.552@studms.ug.edu.pl>
2021-03-07 23:16:34 +0100

    7

--- lab2/zadanie7.js
+++ lab2/zadanie7.js
@@ -1,33 +1,7 @@
 function delet(arr) {
-    for(let i = 0; i < arr.length; i++){
-        switch(arr[i]) {
-            case false:
-                arr.splice(i, 1);
-                break;
-            case null:
-                arr.splice(i, 1);
-                break;
-            case undefined:
-                arr.splice(i, 1);
-                break;
-            case 0:
-                arr.splice(i, 1);
-                break;
-            case "'":
-                arr.splice(i, 1);
-                break;
-            case isNaN(arr[i]):
-                arr.splice(i, 1);
-                break;
-            default:
-                break;
-        }
-    }
-    return arr
+    let arr1 = arr.filter(Boolean);
+    return arr1;
 }
 
 let a = [NaN, 1, "abc", "0", 32, false, undefined];
 console.log(delet(a));
-console.log(a)
-console.log("'")
-
